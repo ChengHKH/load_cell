733712d81dfdffab9bfe284c19a319fd3a5c2e59
ChengHKH <ChengHKH@gmail.com>
2022-09-15 16:43:38 +0100

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use arduino_hal::prelude::*;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    loop {};
 }
