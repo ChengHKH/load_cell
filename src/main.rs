cddc1b3830c94801f3234310ece819ed7e0e929f
ChengHKH <ChengHKH@gmail.com>
2022-09-16 17:01:36 +0100

    spi test

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,33 @@
 #![no_main]
 
 use panic_halt as _;
+use embedded_hal::spi::FullDuplex;
+use arduino_hal::prelude::*;
+use arduino_hal::spi;
+use nb;
+use ufmt;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    let mut led = pins.d13.into_output();
-    led.set_high();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    
+    let (mut spi, _) = arduino_hal::Spi::new(
+        dp.SPI,
+        pins.d13.into_output(),
+        pins.d11.into_output(),
+        pins.d12.into_pull_up_input(),
+        pins.d10.into_output(),
+        spi::Settings::default()
+    );
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(100);
-        led.toggle();
-        arduino_hal::delay_ms(100);
-        led.toggle();
-        arduino_hal::delay_ms(100);
-        led.toggle();
-        arduino_hal::delay_ms(500);
+        nb::block!(spi.send(0b00001111)).void_unwrap();
+        let data = nb::block!(spi.read()).void_unwrap();
+
+        ufmt::uwriteln!(&mut serial, "data: {}\r", data).void_unwrap();
+        arduino_hal::delay_ms(1000);
     }
 }
