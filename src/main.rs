e5081af5c5f73e2ad7eedfe7b1d430efd64a1876
ChengHKH <ChengHKH@gmail.com>
2022-09-16 13:47:13 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,23 @@
 #![no_main]
 
 use panic_halt as _;
-use arduino_hal::prelude::*;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    loop {};
+    let mut led = pins.d13.into_output();
+    led.set_high();
+
+    loop {
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(500);
+    }
 }
