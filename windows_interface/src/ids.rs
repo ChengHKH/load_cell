93df59d5157cef593e2ff651d9462edf64418caa
ChengHKH <ChengHKH@gmail.com>
2022-11-14 15:15:22 +0000

    create id file

---
+++ windows_interface/src/ids.rs
@@ -0,0 +1,14 @@
+use winsafe::seq_ids;
+
+seq_ids! {
+    FILE_NEW = 1000;
+}
+
+seq_ids! {
+    OPTIONS_TARE = 2000;
+    OPTIONS_RECALIBRATE
+    OPTIONS_KG
+    OPTIONS_G
+    OPTIONS_LB
+    OPTIONS_N
+}
