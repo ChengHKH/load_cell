dfb6b495b9f521d5e90590bbfdb491f25fc06c1a
ChengHKH <ChengHKH@gmail.com>
2022-11-17 16:50:48 +0000

    remove build script

--- build.rs
+++
@@ -1,7 +0,0 @@
-use std::env::consts;
-
-fn main() {
-    if consts::FAMILY == 'unix' {
-        println!("cargo:rustc-env=CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_LINKER=../../msvc-linker/linker-x64.sh")
-    }
-}
