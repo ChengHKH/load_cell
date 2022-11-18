16e235bca42e0e291e8386a8c2167ae710f806ba
ChengHKH <ChengHKH@gmail.com>
2022-11-18 11:33:19 +0000

    remove build script

--- windows_interface/build.rs
+++
@@ -1,8 +0,0 @@
-use std::env::consts;
-
-#[cfg(target_env = "msvc")]
-fn main() {
-    if consts::OS == "linux" {
-        println!("cargo:rustc-link-arg-bin=windows_interface=../../msvc-linker/linker-x64.sh")
-    }
-}
