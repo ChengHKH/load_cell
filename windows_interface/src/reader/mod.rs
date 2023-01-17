895f23a34d0c38c73e9a4ab52c97c406b9f07fec
ChengHKH <ChengHKH@gmail.com>
2023-01-17 16:22:14 +0000

    move reader events

--- windows_interface/src/reader/mod.rs
+++ windows_interface/src/reader/mod.rs
@@ -1,6 +1,7 @@
-use winsafe::{prelude::*, gui, co};
+use winsafe::{prelude::*, gui};
 
 mod ui;
+mod events;
 
 #[derive(Clone)]
 pub struct Reader {
@@ -9,38 +10,8 @@ pub struct Reader {
 
 impl Reader {
     pub fn new(parent: &impl GuiParent) -> Reader {
-        let new_self = Self::build;
+        let new_self = ui::build(parent);
         new_self.events();
         new_self
-    }
-
-    fn events(&self) {
-        self.window.on().wm_create({
-            let reader_window = self.window.clone();
-            move |_| {
-                reader_window.hwnd().SetTimer(1, 1000, None)?;
-                Ok(0)
-            }
-        });
-
-        self.window.on().wm_paint({
-            let reader_window = self.window.clone();
-            move || {
-                Self::draw_reading(reader_window.hwnd(), get_reading())?;
-                Ok(())
-            }
-        });
-
-        self.window.on().wm_timer(1, {
-            let reader_window = self.window.clone();
-            move || {
-                reader_window.hwnd().RedrawWindow(
-                    &reader_window.hwnd().GetClientRect()?,
-                    Handle::NULL,
-                    co::RDW::INVALIDATE | co::RDW::UPDATENOW,
-                )?;
-                Ok(())
-            }
-        });
     }
 }
