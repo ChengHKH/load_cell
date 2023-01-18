0c6a648468bd04062c18110b03c5d0fa8082239b
unknown <ChengHKH@gmail.com>
2023-01-18 18:41:15 +0000

    update DlgDropDown event to use new struct

--- windows_interface/src/dlg/events.rs
+++ windows_interface/src/dlg/events.rs
@@ -4,29 +4,29 @@ use super::ui;
 
 impl ui::DlgDropDown {
     pub fn events(&self) {
-        self.dialog.buttons.button_one.unwrap().on().bn_clicked({
+        self.buttons.button_one.unwrap().on().bn_clicked({
             let self2 = self.clone();
             move || {
                 *self2.return_value.try_borrow_mut()? = Some(self2.drop_down.text());
-                self2.dialog.window.hwnd().EndDialog(0)?;
+                self2.window.hwnd().EndDialog(0)?;
                 Ok(())
             }
         });
 
-        self.dialog.buttons.button_two.unwrap().on().bn_clicked({
+        self.buttons.button_two.unwrap().on().bn_clicked({
             let self2 = self.clone();
             move || {
                 *self2.return_value.try_borrow_mut()? = None;
-                self2.dialog.window.hwnd().EndDialog(0)?;
+                self2.window.hwnd().EndDialog(0)?;
                 Ok(())
             }
         });
 
-        self.dialog.window.on().wm_command_accel_menu(co::DLGID::CANCEL.into(), {
+        self.window.on().wm_command_accel_menu(co::DLGID::CANCEL.into(), {
             let self2 = self.clone();
             move || {
                 *self2.return_value.try_borrow_mut()? = None;
-                self2.dialog.window.hwnd().EndDialog(0)?;
+                self2.window.hwnd().EndDialog(0)?;
                 Ok(())
             }
         });
